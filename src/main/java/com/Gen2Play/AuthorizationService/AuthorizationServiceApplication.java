f3dd802d1e46052038a22bdf8861620fa558ccab
Vo Thanh Tuyen <thanhtuyen66202@gmail.com>
2025-02-24 00:41:06 +0700

    add Docker file and remove h2 db

--- src/main/java/com/Gen2Play/AuthorizationService/AuthorizationServiceApplication.java
+++ src/main/java/com/Gen2Play/AuthorizationService/AuthorizationServiceApplication.java
@@ -4,15 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 @EnableDiscoveryClient
 public class AuthorizationServiceApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		// Dotenv dotenv = Dotenv.load();
+		// dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(AuthorizationServiceApplication.class, args);
 	}
 
